4b33e541122538cf9d57fd41be46f0516baff55e
MarlonWiese <106740980+acenixton@users.noreply.github.com>
2024-12-02 20:55:19 +0100

    works, just not very fancy

--- GUI_tests/chase_button_PyQt.py
+++ GUI_tests/chase_button_PyQt.py
@@ -2,30 +2,35 @@
 # A small GUI game, where the button moves every time it is pressed
 # This time with PyQt because tkinter is annoying
 
-
 import PyQt6.QtWidgets as wi
-from PyQt6.QtCore import QSize
 import sys
+from random import *
+
+width = 900
+height = 900
+bump = 140
+sx = width-bump
+sy = height-bump
 
 def jump():
-    
+    buJump.move(randint(bump,sx),randint(bump,sy))
+def end():
+    sys.exit()
         
 app = wi.QApplication(sys.argv)
 win = wi.QWidget()
-win.setGeometry(100,100,900,900)
+win.setGeometry(100,100,width,height)
+win.setWindowTitle("Chase the Button!")
 
 buJump = wi.QPushButton("jump")
-buJump.setFixedWidth(90)
+buJump.setFixedSize(100,50)
 buJump.clicked.connect(jump)
 
 layout = wi.QVBoxLayout(win)
 layout.addWidget(buJump)
 
-
-win.setContentsMargins(20,20,20,20)
-
+win.setContentsMargins(50,20,50,20)
 win.show()
-
 sys.exit(app.exec())
 
 
